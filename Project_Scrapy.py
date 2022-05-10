79eb4f5d649f564b213e4c0963d2a1bd883b3122
Dawid <82048467+sunnyline99@users.noreply.github.com>
2022-05-10 23:12:50 +0200

    Add files via upload

---
+++ Project_Scrapy.py
@@ -0,0 +1,48 @@
+import scrapy
+class Info(scrapy.Item):
+    player = scrapy.Field()
+    wins = scrapy.Field()
+    losses = scrapy.Field()
+    date = scrapy.Field()
+
+class Other(scrapy.Item):
+    x = scrapy.Field()
+
+class ProjectSpider(scrapy.Spider):
+    name = "basic_info"
+    allowed_domains = ["www.goratings.org"]
+
+    try:
+        with open("link_lists.csv", "rt") as f:
+            start_urls = [url.strip() for url in f.readlines()][1:]
+    except:
+        start_urls = []
+
+
+
+    def parse(self, response):
+            p = Info()
+            player_xpath = '//h1/text()'
+            date_xpath = '//th[text()="Date of Birth"]/following-sibling::*/text()'
+            wins_xpath = '//th[text()="Wins"]/following-sibling::*/text()'
+            losses_xpath = '//th[text()="Losses"]/following-sibling::*/text()'
+
+            x=[]
+            
+            print('****************************************************')
+            for j in range(2,12):
+                rows = response.xpath('//tr['+str(j)+']/*[3]/text()')
+                for row in rows:
+                    x.append(row.extract())
+                    #print(row.xpath('td/text()').getall())
+                
+            print(x)
+
+
+            p['player'] = response.xpath(player_xpath).getall()
+            p['date'] = response.xpath(date_xpath).getall()
+            p['wins'] = response.xpath(wins_xpath).getall()
+            p['losses'] = response.xpath(losses_xpath).getall()
+
+            yield p
+#scrapy crawl basic_info -o Info.csv -t csv
